a012853563081da7702fe7847e13ca851e22dc50
mdreves <mdreves@google.com>
2018-12-27 12:56:51 -0800

    TFMA - Temporarily expose validator types until TFMV project created.
    PiperOrigin-RevId: 227054943

--- tensorflow_model_analysis/__init__.py
+++ tensorflow_model_analysis/__init__.py
@@ -19,6 +19,7 @@ from tensorflow_model_analysis import view
 from tensorflow_model_analysis import evaluators
 from tensorflow_model_analysis import extractors
 from tensorflow_model_analysis import slicer
+from tensorflow_model_analysis import validators
 from tensorflow_model_analysis import writers
 from tensorflow_model_analysis.api import tfma_unit as test
 
--- tensorflow_model_analysis/validators/__init__.py
+++ tensorflow_model_analysis/validators/__init__.py
@@ -13,3 +13,5 @@
 # limitations under the License.
 """Init module for TensorFlow Model Analysis validators."""
 
+from tensorflow_model_analysis.validators.validator import Validation
+from tensorflow_model_analysis.validators.validator import Validator
